4cd38014f7d0a0048b4b3821fa0b4a2f3463b4e1
Danila Tkachenko <olrait9@gmail.com>
2025-07-29 11:26:39 +0200

    логин и авторизация токеном JWT

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,16 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Get, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Post('register')
-    async register(@Body() dto: CreateUserDto) {
-        const user = await this.userService.createUser(dto);
-        // Лучше не возвращать пароль даже захешированный
-        const { password, ...result } = user;
-        return result;
-    }
+  @Post('register')
+  async register(@Body() dto: CreateUserDto) {
+    const user = await this.userService.createUser(dto);
+    const { password, ...result } = user;
+    return result;
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Req() req) {
+    return req.user;
+  }
 }
